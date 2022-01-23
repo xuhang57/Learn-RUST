87872fdb9a280299c568c1be77a29d269ee101a0
Lucas Xu <xuhang57@users.noreply.github.com>
2022-01-23 13:17:24 -0800

    Complete chapter 10 and start chapter 11

--- Chapter10/generics.rs
+++ Chapter10/generics.rs
@@ -51,3 +51,78 @@ fn largest<T>(list: &[T]) -> T {
 }
 
 // Generics in Struct Definitions
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let integer = Point {x: 5, y: 10};
+    let float = Point {x: 1.0, y: 4.0};
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+// In Method Definitions
+
+Struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let p = Point {x: 5, y: 10};
+    println!("p.x = {}", p.x());
+}
+
+// We could implement methods only on Point<f32> instances rather than on Point<T>
+impl Point<f32> {
+    fn distance_from_orgin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// A method that uses different generic types from its struct's definition
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point {x: 5, y: 10.4};
+    let p2 = Point {x: "Hello", y: 'c'};
+
+    let p3  = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+
