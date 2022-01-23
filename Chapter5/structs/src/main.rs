32779ff0a388d368d778b6313dfac47e289af330
Lucas H. Xu <xuhang57@users.noreply.github.com>
2022-01-23 13:14:12 -0800

    update chapter5 and 8

--- Chapter5/structs/src/main.rs
+++ Chapter5/structs/src/main.rs
@@ -173,7 +173,7 @@ has this behavior.
 p1.distance(&p2); // auto referencing
 (&p1).distance(&p2);
 
-By using struct, you can keep associated pieces of data connected  to each other and name each piece to make your code clear
+By using structs, you can keep associated pieces of data connected  to each other and name each piece to make your code clear
 Structs are not the only way you can create custom types; Rust has enum feature as well
 */
 
